98a48667902b78a828a98eccfa82190eb99e0509
Jae Young Ryoo <jay.ryoo@gmail.com>
2012-04-09 16:05:15 -0700

    udated HMM.py, started forward.py

--- HMM.py
+++ HMM.py
@@ -13,4 +13,13 @@ class HMM:
         return self.parameters[e][k][b]
     def p(self, k):
         return self.marginal[k]
-    
+    def getNumHiddenStates(self):
+        return len(self.stateSpace)
+    def getE(self):
+        return self.emission
+    def getS(self):
+        return self.stateSpace
+    def getParameters(self):
+        return self.parameters
+    def getMaarginal(self):
+        return self.marginal
